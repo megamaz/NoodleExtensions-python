e2ead0fe41be2263fe531d9aba836ab078a70cb5
megamaz <44149188+megamaz@users.noreply.github.com>
2020-10-20 13:34:43 -0700

    Intro text lol

--- main.py
+++ main.py
@@ -97,3 +97,23 @@ def OpenLevel(wipLevelFolder, difficulty, charact='Standard'):
         else:
             with open(rf'{data["CustomWIPLevelsPath"]}\{wipLevelFolder}\{difficulty}{charact}.dat', 'r') as GetDiff:
                 return json.load(GetDiff)
+print('''
+PPPPPPPPPPPPPPPPP   NNNNNNNN        NNNNNNNNEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+P::::::::::::::::P  N:::::::N       N::::::NE::::::::::::::::::::EE::::::::::::::::::::E
+P::::::PPPPPP:::::P N::::::::N      N::::::NE::::::::::::::::::::EE::::::::::::::::::::E
+PP:::::P     P:::::PN:::::::::N     N::::::NEE::::::EEEEEEEEE::::EEE::::::EEEEEEEEE::::E
+  P::::P     P:::::PN::::::::::N    N::::::N  E:::::E       EEEEEE  E:::::E       EEEEEE
+  P::::P     P:::::PN:::::::::::N   N::::::N  E:::::E               E:::::E             
+  P::::PPPPPP:::::P N:::::::N::::N  N::::::N  E::::::EEEEEEEEEE     E::::::EEEEEEEEEE   
+  P:::::::::::::PP  N::::::N N::::N N::::::N  E:::::::::::::::E     E:::::::::::::::E   
+  P::::PPPPPPPPP    N::::::N  N::::N:::::::N  E:::::::::::::::E     E:::::::::::::::E   
+  P::::P            N::::::N   N:::::::::::N  E::::::EEEEEEEEEE     E::::::EEEEEEEEEE   
+  P::::P            N::::::N    N::::::::::N  E:::::E               E:::::E             
+  P::::P            N::::::N     N:::::::::N  E:::::E       EEEEEE  E:::::E       EEEEEE
+PP::::::PP          N::::::N      N::::::::NEE::::::EEEEEEEE:::::EEE::::::EEEEEEEE:::::E
+P::::::::P          N::::::N       N:::::::NE::::::::::::::::::::EE::::::::::::::::::::E
+P::::::::P          N::::::N        N::::::NE::::::::::::::::::::EE::::::::::::::::::::E
+PPPPPPPPPP          NNNNNNNN         NNNNNNNEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE    
+
+A Python based Beat Saber Noodle Extensions Editor (Python Noodle Extensions Editor)
+''')
