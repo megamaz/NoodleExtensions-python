0b115d5e4d90e69d523158e7036ee424b95264e0
megamaz <44149188+megamaz@users.noreply.github.com>
2020-10-20 13:31:49 -0700

    Level Loading

--- main.py
+++ main.py
@@ -13,9 +13,10 @@
 # PP::::::PP          N::::::N      N::::::::NEE::::::EEEEEEEE:::::EEE::::::EEEEEEEE:::::E
 # P::::::::P          N::::::N       N:::::::NE::::::::::::::::::::EE::::::::::::::::::::E
 # P::::::::P          N::::::N        N::::::NE::::::::::::::::::::EE::::::::::::::::::::E
-# PPPPPPPPPP          NNNNNNNN         NNNNNNNEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE                          
-# Python Noodle Extensions Editor. I can't do ASCII, so I used http://patorjk.com/software/taag/#p=testall&h=0&v=0&f=Alpha&t=PNEE "Doh" (Pronounced the same as Knee, /nē/)
-
+# PPPPPPPPPP          NNNNNNNN         NNNNNNNEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE    
+#                       
+# Python Noodle Extensions Editor. (Great name, I know.) I can't do ASCII, so I used http://patorjk.com/software/taag/#p=testall&h=0&v=0&f=Alpha&t=PNEE "Doh" (Pronounced the same as Knee, /nē/)
+# This code is awfully simple. Please improve it.
 
 import json, argparse, os
 from pathlib import Path
@@ -40,10 +41,59 @@ def Setup():
 
             print("Everything has been setup. Please restart the project.")
             json.dump(data, CreateWrite)
+    return data
 
 if args.setup:
     if not os.path.exists(GetLocalPath('data.json')):
-        Setup()
+        data = Setup()
     else:
         os.remove(GetLocalPath('data.json')) # Delete the file and do setup again.
-        Setup()
+        data = Setup()
+else:
+    with open(GetLocalPath('data.json'), 'r') as f:
+        data = json.load(f)
+# Editor time!
+def OpenLevel(wipLevelFolder, difficulty, charact='Standard'):
+    # Found out lately that functions use MarkDown for their description. I'm a god now.
+    '''
+    # OpenLevel
+    .dat files are just renamed .json files.
+    returns: `LEVEL.dat` as a Dictionary
+    - wipLevelFolder\n
+    Not the `CustomWIPLevels`, but the folder containing the `LEVEL.dat` and `info.dat`. 
+    - difficulty\n
+    The difficulty (ranging from Easy to ExpertPlus)
+    type: `str`
+    - charact
+    The type (Defaults to 'standard')
+    '''
+    global data
+    if not os.path.exists(rf'{data["CustomWIPLevelsPath"]}\{wipLevelFolder}'):
+        raise FileNotFoundError("This level does not exist.")
+    else:
+        with open(f'{data["CustomWIPLevelsPath"]}\\{wipLevelFolder}\\info.dat', 'r') as GetReq:
+            reqs = json.load(GetReq)
+        
+        with open(f'{data["CustomWIPLevelsPath"]}\\{wipLevelFolder}\\info.dat', 'w') as AddReq:
+            for levels in range(len(reqs["_difficultyBeatmapSets"])):
+                if reqs["_difficultyBeatmapSets"][levels]["_beatmapCharacteristicName"] == charact:
+                    for diffs in range(len(reqs["_difficultyBeatmapSets"][levels]["_difficultyBeatmaps"])):
+                        if reqs["_difficultyBeatmapSets"][levels]["_difficultyBeatmaps"][diffs]["_difficulty"] ==  difficulty: # jesus fuck
+                            if 'Noodle Extensions' not in reqs["_difficultyBeatmapSets"][levels]["_difficultyBeatmaps"][diffs]["_customData"]["_requirements"]:
+                                reqs["_difficultyBeatmapSets"][levels]["_difficultyBeatmaps"][diffs]["_customData"]["_requirements"].append('Noodle Extensions') # Do note this is JUST to add the requirement of Noodle Extensions automatically.
+                                break
+            json.dump(reqs, AddReq)
+        
+        if not os.path.exists(rf'{data["CustomWIPLevelsPath"]}\{wipLevelFolder}\{difficulty}{charact}.dat'):
+            if not os.path.exists(rf'{data["CustomWIPLevelsPath"]}\{wipLevelFolder}\{difficulty}.dat'):
+                if not os.path.exists(rf'{data["CustomWIPLevelsPath"]}\{wipLevelFolder}\{difficulty}Standard.dat'):
+                    raise FileNotFoundError("This difficulty does not exist.")
+                else:
+                    with open(rf'{data["CustomWIPLevelsPath"]}\{wipLevelFolder}\{difficulty}Standard.dat', 'r') as GetDiff:
+                        return json.load(GetDiff)
+            else:
+                with open(rf'{data["CustomWIPLevelsPath"]}\{wipLevelFolder}\{difficulty}.dat', 'r') as GetDiff:
+                    return json.load(GetDiff)
+        else:
+            with open(rf'{data["CustomWIPLevelsPath"]}\{wipLevelFolder}\{difficulty}{charact}.dat', 'r') as GetDiff:
+                return json.load(GetDiff)
