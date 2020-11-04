ea7f7f2cd684fc7310511bb63fb2bd90ee476ad4
megamaz <44149188+megamaz@users.noreply.github.com>
2020-11-03 19:02:09 -0800

    Added Track animator's position

--- NoodleExtensions.py
+++ NoodleExtensions.py
@@ -26,10 +26,42 @@ PATHSWINDOWS = { # A list of internal Beat Saber download paths.
     "Steam":r"C:\Program Files (x86)\Steam\steamapps\common\Beat Saber",
     "Oculus":r"C:\OculusApps\Software\hyperbolic-magnematism-beat-saber"
 }
-
+EASINGSNET = "https://easings.net"
+EASINGS = [
+    "easeInsine",
+    "easeOutSine",
+    "easeInOutSine",
+    "easeInCubic",
+    "easeOutCubic",
+    "easeInOutCubic",
+    "easeInQuint",
+    "easeOutQuint",
+    "easeInOutQuint",
+    "easeInCirc",
+    "easeOutCirc",
+    "easeInOutCirc",
+    "easeInElastic",
+    "easeOutElastic",
+    "easeInOutElastic",
+    "easeInQuad",
+    "easeOutQuad",
+    "easeInOutQuad",
+    "easeInQuart",
+    "easeOutQuart",
+    "easeInOutQuart",
+    "easeInExpo",
+    "easeOutExpo",
+    "easeInOutExpo",
+    "easeInBack",
+    "easeOutBack",
+    "easeInOutBack",
+    "easeInBounce",
+    "easeOutBounce",
+    "easeInOutBounce"
+]   
 # Need someone to add a PATHSLINUX. I own windows and do not know where these are.
 
-class NoodleExtensions():
+class Editor():
     
     def __init__(self, CustomLevelPath):
         '''
@@ -56,9 +88,10 @@ class NoodleExtensions():
         '''
         with open(self.CustomLevelPath, 'r') as editnote:
             notes = json.load(editnote)
+        with open(self.CustomLevelPath, 'w') as editnote_:
             for x in range(len(notes["_notes"])):
                 if notes[x]["_time"] == beat and notes[x]["_lineIndex"] == index and notes[x]["_lineLayer"] == layer:
-                    false = False if not interactable else True
+                    false = False if not interactable else True # "Do note that if `interactable` is set to False, `false` will also be set to False. You don't want a block that will kill the player that cannot be hit."
                     if track == None:
                         notes[x]["_customData"] = {
                             "_fake" : false,
@@ -70,3 +103,47 @@ class NoodleExtensions():
                             "_interactable" : interactable,
                             "_track" : track
                         }
+            json.dump(notes, editnote_)
+
+
+class TrackAnimator():
+
+    def __init__(self, editor:Editor):
+        '''just put in the Editor object you're using.'''
+        self.editor = editor # this is as to be able to access the actual level.dat file.
+    
+    def position(self, pos:list, track, start, end):
+        '''
+        - `pos` (list) that should look something like this;
+        ```
+        [
+            [x, y, z, time (beat), ease (optional)],
+            [x, y, z, time (beat), ease (optional)],
+            ...
+        ]
+        ``` It will be used to animate the blocks in the track.
+        - x     : LEFT/RIGHT
+        - y     : UP/DOWN
+        - z     : FORWARDS/BACKWARDS
+        - time  : The beat where the animation should start
+        - ease  : easings. The speed which the note should move between animations. (can be gained from NoodleExtensions.EASINGSNET)
+
+        - `track` the track you want to animate.
+        - `start` the start (in beats) where the animation should start
+        - `end` the end (in beats) where the animation should end.
+        '''
+
+        with open(self.editor.CustomLevelPath, 'r') as GetCustomEvents:
+            ce = json.load(GetCustomEvents)
+        with open(self.editor.CustomLevelPath, 'w') as EditCustomEvents:
+            ce["_customEvents"].append(
+                {
+                    "_time":start,
+                    "_type":"AnimateTrack",
+                    "_data":{
+                        "_duration": end-start,
+                        "_position":pos
+                    }
+                }
+            )
+            json.dump(ce, EditCustomEvents)
