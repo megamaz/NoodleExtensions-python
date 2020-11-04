1114665e5898424c03135f3bf697be988ff366ec
megamaz <44149188+megamaz@users.noreply.github.com>
2020-11-03 17:14:20 -0800

    Making this a library

--- NoodleExtensions.py
+++ NoodleExtensions.py
@@ -18,59 +18,55 @@
 # Python Noodle Extensions Editor. (Great name, I know.) I can't do ASCII, so I used http://patorjk.com/software/taag/#p=testall&h=0&v=0&f=Alpha&t=PNEE "Doh" (Pronounced the same as Knee, /nē/)
 # This code is awful. Please improve it.
 
-import json, os, argparse
+import json, os
 from pathlib import Path
 
-def GetLocalPath(filename):
-    return Path(__file__).parents[0] / filename # I still don't understand how this trick works. but it works. im fine with that.
 
+PATHSWINDOWS = { # A list of internal Beat Saber download paths.
+    "Steam":r"C:\Program Files (x86)\Steam\steamapps\common\Beat Saber",
+    "Oculus":r"C:\OculusApps\Software\hyperbolic-magnematism-beat-saber"
+}
 
-if not os.path.exists(GetLocalPath('data.json')):
-    with open('data.json', 'w', encoding="utf-8") as CreateDataJson:
-        print(r"""Please insert your Beat Saber install directory. Here are some locations for you to try;
-Steam
-- C:\Program Files (x86)\Steam\steamapps\common\Beat Saber
+# Need someone to add a PATHSLINUX. I own windows and do not know where these are.
 
-Oculus
-- C:\OculusApps\Software\hyperbolic-magnematism-beat-saber
+class NoodleExtensions():
+    
+    def __init__(self, CustomLevelPath):
+        '''
+        - `CustomLevelPath` The path where the `level.dat` is. 
+        '''
+        if not os.path.exists(CustomLevelPath):
+            raise FileNotFoundError("This level.dat file does not exist.")
+        self.CustomLevelPath = CustomLevelPath
+    
+    def EditBlock(self, beat, index, layer, track=None, false=False, interactable=True):
+        '''
+        Edits a specific block/note (same thing)
+        - `beat` The beat at which the block can be found.
+        - `index` the "_lineIndex" property of the block, or the X position. (0 is left-most)
+        - `layer` the "_lineLayer" proeprty of the block, or the Y position. (0 is bottom)
 
-IF YOU ARE USING QUEST / STANDALONE HEADSETS.
-Just input NONE. You can also use this if you'd rather have a separate WIP levels location than the ingame one.
-""")
-        data = {}
-        beatSaberDir = input(r"> ")
-        if beatSaberDir.lower() != 'none': 
-            if os.path.exists(beatSaberDir + '\\Beat Saber.exe'): # shit way of checking if that's a beat saber install. I hate it. but it works.
-                data["customWIPLevelsPath"] = beatSaberDir + '\\Beat Saber_data\\CustomWIPLevels'
-            else:
-                raise FileNotFoundError("Not a Beat Saber install path.")
-        else:
-            data["customWIPLevelsPath"] = input("insert a path for your custom levels: ")
-        
+        ### To edit the block
+        - `track` add / change the track of a block. (ignore this to remove the track.)
+        - `false` whether or not the block will affect the player's score.
+        - `interactable` whether or not the saber can go through the block.
 
-        json.dump(data, CreateDataJson)
-parser = argparse.ArgumentParser(description='The thing that takes care of Noodle Extensioning.')
-parser.add_argument('-f', '--file')
-args = parser.parse_args()
-
-if args.file == None:
-    print("""
-PPPPPPPPPPPPPPPPP   NNNNNNNN        NNNNNNNNEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-P::::::::::::::::P  N:::::::N       N::::::NE::::::::::::::::::::EE::::::::::::::::::::E
-P::::::PPPPPP:::::P N::::::::N      N::::::NE::::::::::::::::::::EE::::::::::::::::::::E
-PP:::::P     P:::::PN:::::::::N     N::::::NEE::::::EEEEEEEEE::::EEE::::::EEEEEEEEE::::E
-  P::::P     P:::::PN::::::::::N    N::::::N  E:::::E       EEEEEE  E:::::E       EEEEEE
-  P::::P     P:::::PN:::::::::::N   N::::::N  E:::::E               E:::::E             
-  P::::PPPPPP:::::P N:::::::N::::N  N::::::N  E::::::EEEEEEEEEE     E::::::EEEEEEEEEE   
-  P:::::::::::::PP  N::::::N N::::N N::::::N  E:::::::::::::::E     E:::::::::::::::E   
-  P::::PPPPPPPPP    N::::::N  N::::N:::::::N  E:::::::::::::::E     E:::::::::::::::E   
-  P::::P            N::::::N   N:::::::::::N  E::::::EEEEEEEEEE     E::::::EEEEEEEEEE   
-  P::::P            N::::::N    N::::::::::N  E:::::E               E:::::E             
-  P::::P            N::::::N     N:::::::::N  E:::::E       EEEEEE  E:::::E       EEEEEE
-PP::::::PP          N::::::N      N::::::::NEE::::::EEEEEEEE:::::EEE::::::EEEEEEEE:::::E
-P::::::::P          N::::::N       N:::::::NE::::::::::::::::::::EE::::::::::::::::::::E
-P::::::::P          N::::::N        N::::::NE::::::::::::::::::::EE::::::::::::::::::::E
-PPPPPPPPPP          NNNNNNNN         NNNNNNNEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-
-a Python based Noodle Extensions "Editor".
-    """)
+        Do note that if `interactable` is set to False, `false` will also be set to False. 
+        You don't want a block that will kill the player that cannot be hit. 
+        '''
+        with open(self.CustomLevelPath, 'r') as editnote:
+            notes = json.load(editnote)
+            for x in range(len(notes["_notes"])):
+                if notes[x]["_time"] == beat and notes[x]["_lineIndex"] == index and notes[x]["_lineLayer"] == layer:
+                    false = False if not interactable else True
+                    if track == None:
+                        notes[x]["_customData"] = {
+                            "_fake" : false,
+                            "_interactable" : interactable
+                        }
+                    else:
+                        notes[x]["_customData"] = {
+                            "_fake" : false,
+                            "_interactable" : interactable,
+                            "_track" : track
+                        }
